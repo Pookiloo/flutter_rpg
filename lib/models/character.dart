93f382072403ea3cda11dc3201f23c249e66beee
Reghan <reghan.morrow@gmail.com>
2025-03-31 17:54:46 -0400

    Lesson 28: UPDATED

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -5,15 +5,15 @@ class Character {
     required this.name,
     required this.slogan,
     required this.id,
-  })
+  });
 
   //fields
   final String name;
   final String slogan;
   final String id;
-  bool iFav = false;
+  bool _isFav = false;
 
-  void toggleIsFav({
+  void toggleIsFav(){
     _isFav = !_isFav;
-  })
+  }
 }
